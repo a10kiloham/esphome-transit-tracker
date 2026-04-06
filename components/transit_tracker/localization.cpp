4fd3c11a8fdcbb7acc43c0ffaf1c43cb1e984817
a10kiloham <295361+a10kiloham@users.noreply.github.com>
2026-04-06 09:01:42 +0000

    Due sign

--- components/transit_tracker/localization.cpp
+++ components/transit_tracker/localization.cpp
@@ -6,19 +6,8 @@ namespace transit_tracker {
 std::string Localization::fmt_duration_from_now(time_t unix_timestamp, uint rtc_now) const {
   int diff = unix_timestamp - rtc_now;
 
-  if (diff < 30) {
-    return this->now_string_;
-  }
-
   if (diff < 60) {
-    switch (this->unit_display_) {
-      case UNIT_DISPLAY_LONG:
-        return "0" + this->minutes_long_string_;
-      case UNIT_DISPLAY_SHORT:
-        return "0" + this->minutes_short_string_;
-      case UNIT_DISPLAY_NONE:
-        return "0";
-    }
+    return this->now_string_;
   }
 
   int minutes = diff / 60;
